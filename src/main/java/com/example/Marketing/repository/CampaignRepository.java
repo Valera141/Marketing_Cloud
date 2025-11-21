a5d057769719b4382a6728a2fb59f61f1fee46a9
Valera141 <152248303+Valera141@users.noreply.github.com>
2025-11-20 23:13:36 -0600

    Add files via upload

---
+++ src/main/java/com/example/Marketing/repository/CampaignRepository.java
@@ -0,0 +1,10 @@
+package com.example.Marketing.repository;
+
+import com.example.Marketing.model.Campaign;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CampaignRepository extends JpaRepository<Campaign, Integer> {
+    // Spring Data JPA crea los métodos automáticamente
+}   
